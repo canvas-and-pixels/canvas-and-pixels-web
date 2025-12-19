f3985dfb86e36ee79887955e07c6efa4c7a0ccc2
Collins-01 <oriakhicolls01@gmail.com>
2025-12-19 11:17:38 +0100

    fix: move CollaborationHero below FAQ section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
-import CollaborationHero from '@/components/CollaborationHero';
 import WorksSection from '@/components/WorksSection';
 import ServicesSection from '@/components/ServicesSection';
 import WhyChooseSection from '@/components/WhyChooseSection';
 import FAQSection from '@/components/FAQSection';
+import CollaborationHero from '@/components/CollaborationHero';
 import ContactModal from '@/components/ContactModal';
 
 export default function Home() {
@@ -14,11 +14,11 @@ export default function Home() {
       <Header />
       <HeroSection />
       <AboutSection />
-      <CollaborationHero />
       <WorksSection />
       <ServicesSection />
       <WhyChooseSection />
       <FAQSection />
+      <CollaborationHero />
       <ContactModal />
     </main>
   );
